0eadf36f43b67f6007b6499b2c56223e395b168f
subhrajit <subhrajit@gmail.com>
2021-08-03 14:45:26 -0400

    added utils.hpp under tests

---
+++ tests/utils.hpp
@@ -0,0 +1,28 @@
+/** **************************************************************************************
+*                                                                                        *
+*    A Ridiculously Simple JSON Parser for C++ (RSJp-cpp)                                *
+*    Version 2.x                                                                         *
+*    ----------------------------------------------------------                          *
+*    Copyright (C) 2018  Subhrajit Bhattacharya                                          *
+*                                                                                        *
+*    This program is free software: you can redistribute it and/or modify                *
+*    it under the terms of the GNU General Public License as published by                *
+*    the Free Software Foundation, either version 3 of the License, or                   *
+*    (at your option) any later version.                                                 *
+*                                                                                        *
+*    This program is distributed in the hope that it will be useful,                     *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of                      *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                       *
+*    GNU General Public License for more details <http://www.gnu.org/licenses/>.         *
+*                                                                                        *
+*                                                                                        *
+*    Contact:  subhrajit@gmail.com                                                       *
+*              https://www.lehigh.edu/~sub216/ , http://subhrajit.net/                   *
+*                                                                                        *
+*                                                                                        *
+*************************************************************************************** **/
+
+// RSJP header:
+#include "RSJparser.tcc"
+
+RSJresource read_file (std::string fname);
